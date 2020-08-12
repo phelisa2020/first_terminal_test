a959ca9d7a091062a1a525c684dc88666f56da96
phelisa2020 <phelisagqwetha@gmail.com>
2020-08-12 14:27:02 +0200

    my coomits

--- test/mostProfitableDepartment.test.js
+++ test/mostProfitableDepartment.test.js
@@ -2,7 +2,6 @@ let assert = require("assert");
 let mostProfitableDepartment = require("../mostProfitableDepartment");
 
 describe('mostProfitableDepartment' , function(){
-  it('should return  the most profitable department ' , function(){ 
    var salesData = [
     {department: 'hardware', sales: 4500, day: 'Monday'},
     {department: 'outdoor', sales: 1500, day: 'Monday'},
@@ -20,29 +19,16 @@ describe('mostProfitableDepartment' , function(){
     {department: 'outdoor', sales: 12006, day: 'Friday'},
     {department: 'carpentry', sales: 16109, day: 'Friday'}
 ];
-
-    assert.equal(salesData, mostProfitableDepartment(salesData, 10));
+   
+   var department = ''
+  it('should return  the most profitable department ' , function(){ 
+  
+    assert.equal(department, mostProfitableDepartment(salesData));
   });
   it('should return false if it is not the most profitable department', function(){
-   var salesData = [
-    {department: 'hardware', sales: 4500, day: 'Monday'},
-    {department: 'outdoor', sales: 1500, day: 'Monday'},
-    {department: 'carpentry', sales: 5500, day: 'Monday'},
-    {department: 'hardware', sales: 7500, day: 'Tuesday'},
-    {department: 'outdoor', sales: 2505, day: 'Tuesday'},
-    {department: 'carpentry', sales: 1540, day: 'Tuesday'},
-    {department: 'hardware', sales: 1500, day: 'Wednesday'},
-    {department: 'outdoor', sales: 8507, day: 'Wednesday'},
-    {department: 'carpentry', sales: 8009, day: 'Wednesday'},
-    {department: 'hardware', sales: 12000, day: 'Thursday'},
-    {department: 'outdoor', sales: 18007, day: 'Thursday'},
-    {department: 'carpentry', sales: 6109, day: 'Thursday'},
-    {department: 'hardware', sales: 7005, day: 'Friday'},
-    {department: 'outdoor', sales: 12006, day: 'Friday'},
-    {department: 'carpentry', sales: 16109, day: 'Friday'}
-];
+   
 
-   assert.equal(false, mostProfitableDepartment(salesData, 2));
+   assert.equal(false, mostProfitableDepartment(salesData));
   });
   
 });
